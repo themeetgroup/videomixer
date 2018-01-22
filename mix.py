46692acbd5e6c149403c5bdf0f789c552ce24fac
Misha Nasledov <mnasledov@themeetgroup.com>
2018-01-22 11:31:04 -0800

    add a networking subroutine

--- mix.py
+++ mix.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
+import asyncio
+import gbulb
 import sys
-import rtmpsource
-import videomixer
 import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstBase', '1.0')
 gi.require_version('Gtk', '3.0')
-from gi.repository import GObject, Gst, GstBase, Gtk, GObject, GLib
-
+from gi.repository import GObject, Gst, GstBase, GObject, GLib
+import rtmpsource
+import videomixer
 
 class Mix:
     input_test_url = 'rtmp://stream-0-stage.taggedvideo.com/live/mishatest1'
@@ -27,6 +28,20 @@ class Mix:
         self.video1 = self.videomix.add_rtmp_source(self.input_test_url, 20, 20, 10, 360, 640)
         self.video2 = self.videomix.add_rtmp_source(self.input_test_url2, 440, 20, 30, 180, 320)
         self.focus = False
+
+        GLib.timeout_add(5000, self.move_videos)
+
+        gbulb.install()
+        self.play()
+        server = asyncio.start_server(self.client_connected_handler, 'localhost', 8888)
+        asyncio.get_event_loop().run_until_complete(server)
+        asyncio.get_event_loop().run_forever()
+
+    @asyncio.coroutine
+    def client_connected_handler(self, client_reader, client_writer):
+        print("Connection received")
+        client_writer.write(b"Hello\r\n")
+        client_writer.close()
 
     def play(self):
         if self.started is False:
@@ -49,9 +64,3 @@ class Mix:
         return True
 
 start = Mix()
-GLib.timeout_add(5000, start.move_videos)
-start.play()
-Gtk.main()
-# asyncio -- preferred. transforms async event driven code into seq code.
-#            explicit async boundaries. non-blocking i/o server.
-# idle add (Gtk) -- post to main loop using g_idle_add ?
